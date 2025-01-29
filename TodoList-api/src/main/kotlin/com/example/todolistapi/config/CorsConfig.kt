e9c59fac1a49a38cacc8eaaa6d51fed57f7618ca
hunknownn <zhfptm12@gmail.com>
2025-01-29 16:10:24 +0900

    cors origin patterns *

--- TodoList-api/src/main/kotlin/com/example/todolistapi/config/CorsConfig.kt
+++ TodoList-api/src/main/kotlin/com/example/todolistapi/config/CorsConfig.kt
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해
-            .allowedOrigins("*") // 허용할 출처
+            .allowedOriginPatterns("*")
+//            .allowedOrigins("*") // 허용할 출처
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowCredentials(true) // 쿠키 사용 허용 여부
     }
