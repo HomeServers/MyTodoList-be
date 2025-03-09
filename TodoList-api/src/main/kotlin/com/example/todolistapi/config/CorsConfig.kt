f3d4c2d228439c19110dc264538f288a8ae0e8b8
hunknownn <zhfptm12@gmail.com>
2025-03-09 12:35:14 +0900

    origin 허용3

--- TodoList-api/src/main/kotlin/com/example/todolistapi/config/CorsConfig.kt
+++ TodoList-api/src/main/kotlin/com/example/todolistapi/config/CorsConfig.kt
@@ -9,8 +9,6 @@ class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해
             .allowedOrigins("http://211.38.42.9:3000", "http://todo.nuhgnod.site", "https://todo.nuhgnod.site") // local
-            .allowedOrigins("http://todo.nuhgnod.site") // http domain
-            .allowedOrigins("https://todo.nuhgnod.site") // https domain
             .allowedHeaders("*") // 모든 헤더 허용
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowCredentials(true) // 쿠키 사용 허용 여부
