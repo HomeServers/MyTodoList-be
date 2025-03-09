656884fe092d49c8e60f5c4c64e60d79d9ff795a
hunknownn <zhfptm12@gmail.com>
2025-03-09 11:59:09 +0900

    origin 허용

--- TodoList-api/src/main/kotlin/com/example/todolistapi/config/CorsConfig.kt
+++ TodoList-api/src/main/kotlin/com/example/todolistapi/config/CorsConfig.kt
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해
-//            .allowedOriginPatterns("http://localhost:3000","http://211.38.42.9:3000", "*") // 특정 출처 지정
-//            .allowedOriginPatterns("*")
-            .allowedOrigins("http://211.38.42.9:3000") // 허용할 출처
+            .allowedOrigins("http://211.38.42.9:3000") // local
+            .allowedOrigins("http://todo.nuhgnod.site") // http domain
+            .allowedOrigins("https://todo.nuhgnod.site") // https domain
             .allowedHeaders("*") // 모든 헤더 허용
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowCredentials(true) // 쿠키 사용 허용 여부
